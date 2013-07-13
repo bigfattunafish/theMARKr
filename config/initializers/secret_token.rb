c2f28939f97db0c361f6a7609df3090978c947e6
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-07-13 15:11:50 -0400

    First commit. New rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Themarkr::Application.config.secret_token = 'b13394caa35dc542015f0698545f525a57b3f28411af38d956f79bf21ac29dc95acb1a1ec9525518d472700fcd65495edf5274a9a8b605eacc809afb270f412e'
