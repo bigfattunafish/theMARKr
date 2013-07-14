a0a70e0c331a750589f8dfd63f6ac31aab008476
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-07-13 21:59:05 -0400

    Added home_controller and places_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Themarkr::Application.routes.draw do
   root :to => 'home#index', as: 'home'
+  resources :places
 
 
+  #     places GET    /places(.:format)          places#index
+  #            POST   /places(.:format)          places#create
+  #  new_place GET    /places/new(.:format)      places#new
+  # edit_place GET    /places/:id/edit(.:format) places#edit
+  #      place GET    /places/:id(.:format)      places#show
+  #            PUT    /places/:id(.:format)      places#update
+  #            DELETE /places/:id(.:format)      places#destroy
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
