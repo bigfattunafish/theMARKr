1ceaade05735b334b8b6619af6cf5dd1af26a864
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-07-18 15:42:24 -0400

    18 July, 3:40pm

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,35 @@
 class User < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :username
 
   has_many :places
 
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, :length => { :minimum => 2 }
+  validates :username, :length => { :maximum => 14 }
+  validates_format_of :username, :with => /^[A-Za-z\d_]+$/
+
+  validates :password, presence: true, on: :create
+  validates :password, confirmation: true
+  validates :password, :length => { :in => 8..12 }
+  validates_format_of :password, :with => /^[A-Za-z\d_]+$/
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+  def self.authenticate(email, password)
+    user_exist_boolean = User.find_by_email(email)
+    if user_exist_boolean && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user_authenticate_boolean
+    else
+      nil
+    end
+  end
 end
